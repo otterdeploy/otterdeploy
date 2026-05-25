16964ba9edb111b5a1dc79e83410aff4a178d4d4
Jace <arbeitchuka@gmail.com>
2026-05-25 16:10:07 +0200

    refactor(web): wire useAppForm with bound-field registry

--- apps/web/src/features/projects/components/new-resource/form-context.ts
+++ apps/web/src/features/projects/components/new-resource/form-context.ts
@@ -1,4 +1,30 @@
-import { createFormHookContexts } from "@tanstack/react-form";
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+
+import { LinkedSecretsField } from "./form-fields/linked-secrets-field";
+import { NumberField } from "./form-fields/number-field";
+import { PortsField } from "./form-fields/ports-field";
+import { SelectField } from "./form-fields/select-field";
+import { SubmitButton } from "./form-fields/submit-button";
+import { SwitchField } from "./form-fields/switch-field";
+import { TextField } from "./form-fields/text-field";
+import { VariablesField } from "./form-fields/variables-field";
 
 export const { fieldContext, formContext, useFieldContext, useFormContext } =
   createFormHookContexts();
+
+export const { useAppForm, withForm } = createFormHook({
+  fieldContext,
+  formContext,
+  fieldComponents: {
+    TextField,
+    NumberField,
+    SwitchField,
+    SelectField,
+    PortsField,
+    VariablesField,
+    LinkedSecretsField,
+  },
+  formComponents: {
+    SubmitButton,
+  },
+});
